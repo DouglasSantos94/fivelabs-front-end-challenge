d204b514ee47157d23ed57649e60a526dfce1bea
Douglas Santos <dgstos94@gmail.com>
2023-10-10 10:57:41 -0300

    feat: create header layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,28 @@
 import React from "react";
 
 function App() {
-  return <h1>React template</h1>;
+  return (
+    <>
+      <header
+        style={{
+          width: "100%",
+          height: "70px",
+          padding: "0 20px",
+          boxSizing: "border-box",
+          display: "flex",
+          alignItems: "center",
+          justifyContent: "space-between",
+          backgroundColor: "#3A3F44"
+        }}
+      >
+        <h1 style={{ fontSize: "20px", fontWeight: "bold", color: "white" }}>Star Wars Vehicles</h1>
+        <button style={{ backgroundColor: "#3A3F44", borderStyle: "none" }}>
+          <img width="40px" height="38px" src="src/assets/img/cart.png" />
+        </button>
+      </header>
+      <main style={{ width: "100%" }}></main>
+    </>
+  );
 }
 
 export default App;
