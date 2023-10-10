ed83688506e7eb1b1956c201ade86a8281cd26d5
Douglas Santos <dgstos94@gmail.com>
2023-10-10 18:48:01 -0300

    feat: create router

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,24 @@
+import { RouterProvider, createBrowserRouter } from "react-router-dom";
 import Header from "./pages/Header";
 import Home from "./pages/Home";
+import VehicleDetail from "./pages/VehicleDetail";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Home />
+  },
+  {
+    path: "/detail/:id",
+    element: <VehicleDetail />
+  }
+]);
 
 function App() {
   return (
     <>
       <Header />
-      <Home />
+      <RouterProvider router={router} />
     </>
   );
 }
