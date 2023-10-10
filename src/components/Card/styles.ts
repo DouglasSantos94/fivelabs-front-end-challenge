11d98c6ba47da2a3a2ce9863c443c2a2a4ed16af
Douglas Santos <dgstos94@gmail.com>
2023-10-10 15:23:56 -0300

    feat: create Vehicle card component

---
+++ src/components/Card/styles.ts
@@ -0,0 +1,13 @@
+import styled from "styled-components";
+
+const Card = styled.div`
+  background-color: #5b6066;
+  border-radius: 6px;
+  padding: 20px 0;
+`;
+
+export const VehicleCard = styled(Card)`
+  width: 100%;
+  height: 300px;
+  color: white;
+`;
