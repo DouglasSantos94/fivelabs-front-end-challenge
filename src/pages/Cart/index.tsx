40f053216520e319d5440eb5471ebc73360faa38
Douglas Santos <dgstos94@gmail.com>
2023-10-11 10:49:41 -0300

    style: add cart wrapper

--- src/pages/Cart/index.tsx
+++ src/pages/Cart/index.tsx
@@ -1,16 +1,23 @@
+import { Link } from "react-router-dom";
 import { CartCard } from "../../components/Card";
 import { Title } from "../../components/Text";
 import CartItem from "./CartItem";
-import { CartItemsWrapper } from "./styles";
+import { CartItemsWrapper, CartWrapper } from "./styles";
+import { BuyButton } from "../../components/Button";
 
 export default function Cart() {
   return (
-    <CartCard>
-      <CartItemsWrapper>
-        <Title>Carrinho</Title>
-        <CartItem />
-        <CartItem />
-      </CartItemsWrapper>
-    </CartCard>
+    <CartWrapper>
+      <Link to={"/"}>Voltar</Link>
+
+      <CartCard>
+        <CartItemsWrapper>
+          <Title>Carrinho</Title>
+          <CartItem />
+          <CartItem />
+        </CartItemsWrapper>
+        <BuyButton to={"/form"}>Finalizar compra</BuyButton>
+      </CartCard>
+    </CartWrapper>
   );
 }
--- src/pages/Cart/styles.ts
+++ src/pages/Cart/styles.ts
@@ -1,7 +1,15 @@
 import styled from "styled-components";
-
-export const CartItemsWrapper = styled.ul`
-  width: 95%;
+export const CartWrapper = styled.main`
+  width: 100%;
+  padding-top: 20px;
   display: flex;
   flex-direction: column;
+  align-items: center;
+`;
+
+export const CartItemsWrapper = styled.ul`
+  width: 100%;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
 `;
