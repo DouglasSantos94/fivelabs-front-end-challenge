0ba8d1d6743af82ff13749788ec26f8d3481ed0d
Douglas Santos <dgstos94@gmail.com>
2023-10-11 12:19:07 -0300

    style: adjust title align

--- src/components/Text/styles.ts
+++ src/components/Text/styles.ts
@@ -3,6 +3,8 @@ import styled from "styled-components";
 export const Title = styled.p`
   font-size: 36px;
   margin-bottom: 30px;
+  text-align: center;
+  width: 100%;
 `;
 
 export const Paragraph = styled.p`
