e8496368c8493e06ade5b17a7397ccd13485b0e1
Douglas Santos <dgstos94@gmail.com>
2023-10-11 17:23:23 -0300

    feat: create Label component

---
+++ src/components/Label/index.tsx
@@ -0,0 +1,3 @@
+import { Label } from "./styles";
+
+export { Label };
--- src/pages/PaymentForm/index.tsx
+++ src/pages/PaymentForm/index.tsx
@@ -6,6 +6,7 @@ import { formSchema } from "../../schemas/formSchema";
 import { FormDividerWrapper } from "../../components/FormDivider";
 import { FormItem } from "../../components/FormItem";
 import { SearchButton } from "../../components/Button";
+import { Label } from "../../components/Label";
 
 const initialValues = {
   name: "",
@@ -39,31 +40,31 @@ export default function PaymentForm() {
             <Paragraph>Dados pessoais</Paragraph>
             <FormDividerWrapper>
               <FormItem>
-                <label htmlFor="name">Nome:</label>
+                <Label htmlFor="name">Nome:</Label>
                 <Field type="text" id="name" name="name" />
                 <ErrorMessage name="name" component="div" />
               </FormItem>
               <FormItem>
-                <label htmlFor="cpf">CPF:</label>
+                <Label htmlFor="cpf">CPF:</Label>
                 <Field type="text" id="cpf" name="cpf" />
                 <ErrorMessage name="cpf" component="div" />
               </FormItem>
             </FormDividerWrapper>
             <FormDividerWrapper>
               <FormItem>
-                <label htmlFor="phoneNumber">Telefone:</label>
+                <Label htmlFor="phoneNumber">Telefone:</Label>
                 <Field type="text" id="phoneNumber" name="phoneNumber" />
                 <ErrorMessage name="phoneNumber" component="div" />
               </FormItem>
               <FormItem>
-                <label htmlFor="email">E-mail:</label>
+                <Label htmlFor="email">E-mail:</Label>
                 <Field type="email" id="email" name="email" />
                 <ErrorMessage name="email" component="div" />
               </FormItem>
             </FormDividerWrapper>
             <FormDividerWrapper>
               <FormItem>
-                <label htmlFor="zipCode">CEP</label>
+                <Label htmlFor="zipCode">CEP</Label>
                 <Field type="text" id="zipCode" name="zipCode" />
                 <ErrorMessage name="zipCode" component="div" />
               </FormItem>
@@ -71,38 +72,63 @@ export default function PaymentForm() {
             </FormDividerWrapper>
             <FormDividerWrapper>
               <FormItem>
-                <label htmlFor="street">Rua</label>
+                <Label htmlFor="street">Rua</Label>
                 <Field type="text" id="street" name="street" />
                 <ErrorMessage name="street" component="div" />
               </FormItem>
             </FormDividerWrapper>
             <FormDividerWrapper>
               <FormItem>
-                <label htmlFor="number">Nº</label>
+                <Label htmlFor="number">Nº</Label>
                 <Field type="text" id="number" name="number" />
                 <ErrorMessage name="number" component="div" />
               </FormItem>
               <FormItem>
-                <label htmlFor="complement">Complemento</label>
+                <Label htmlFor="complement">Complemento</Label>
                 <Field type="text" id="complement" name="complement" />
                 <ErrorMessage name="complement" component="div" />
               </FormItem>
             </FormDividerWrapper>
             <FormDividerWrapper>
               <FormItem>
-                <label htmlFor="neighborhood">Bairro</label>
+                <Label htmlFor="neighborhood">Bairro</Label>
                 <Field type="text" id="neighborhood" name="neighborhood" />
                 <ErrorMessage name="neighborhood" component="div" />
               </FormItem>
               <FormItem>
-                <label htmlFor="city">Complemento</label>
+                <Label htmlFor="city">Complemento</Label>
                 <Field type="text" id="city" name="city" />
                 <ErrorMessage name="city" component="div" />
               </FormItem>
               <FormItem>
-                <label htmlFor="state">Estado</label>
+                <Label htmlFor="state">Estado</Label>
                 <Field type="text" id="state" name="state" />
                 <ErrorMessage name="state" component="div" />
+              </FormItem>
+            </FormDividerWrapper>
+            <Paragraph>Dados de pagamento</Paragraph>
+            <FormDividerWrapper>
+              <FormItem>
+                <Label htmlFor="cardNumber">Nº do cartão</Label>
+                <Field type="text" id="cardNumber" name="cardNumber" />
+                <ErrorMessage name="cardNumber" component="div" />
+              </FormItem>
+              <FormItem>
+                <Label htmlFor="owner">Titular</Label>
+                <Field type="text" id="owner" name="owner" />
+                <ErrorMessage name="owner" component="div" />
+              </FormItem>
+            </FormDividerWrapper>
+            <FormDividerWrapper>
+              <FormItem>
+                <Label htmlFor="valid">Validade</Label>
+                <Field type="text" id="valid" name="valid" />
+                <ErrorMessage name="valid" component="div" />
+              </FormItem>
+              <FormItem>
+                <Label htmlFor="code">CVV</Label>
+                <Field type="text" id="code" name="code" />
+                <ErrorMessage name="code" component="div" />
               </FormItem>
             </FormDividerWrapper>
           </Form>
