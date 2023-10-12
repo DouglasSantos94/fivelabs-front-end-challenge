841b3dfad489a6444ae02d6ccb69dd5046d07f41
Douglas Santos <dgstos94@gmail.com>
2023-10-12 18:01:19 -0300

    ref: change cart item component props

--- src/pages/Cart/CartItem/index.tsx
+++ src/pages/Cart/CartItem/index.tsx
@@ -1,13 +1,9 @@
 import { RemoveFromCartButton } from "../../../components/Button";
 import { SmallParagraph } from "../../../components/Text";
-import { IVehicle } from "../../../types/Vehicle";
+import { CartItemProps } from "../../../types/Vehicle";
 import { CartItemWrapper } from "./styles";
 
-interface ICartItemProps {
-  product: IVehicle;
-}
-
-export default function CartItem({ product: { name, cost_in_credits: costInCredits } }: ICartItemProps) {
+export default function CartItem({ id, product: { name, cost_in_credits: costInCredits } }: CartItemProps) {
   return (
     <CartItemWrapper>
       <SmallParagraph>Produto: {name}</SmallParagraph>
--- src/pages/Cart/index.tsx
+++ src/pages/Cart/index.tsx
@@ -6,7 +6,7 @@ import { CartWrapper } from "./styles";
 import { BuyButton } from "../../components/Button";
 import { ItemsWrapper } from "../../components/ItemsWrapper";
 import { useStore } from "../../hooks/useStore";
-import { IVehicle } from "../../types/Vehicle";
+import { CartItemProps, IVehicle } from "../../types/Vehicle";
 
 export default function Cart() {
   const {
@@ -20,7 +20,10 @@ export default function Cart() {
       <CartCard>
         <ItemsWrapper>
           <Title>Carrinho</Title>
-          {products && products.map((product: IVehicle, i: number) => <CartItem key={i} product={product} />)}
+          {products &&
+            products.map((cartItem: CartItemProps, i: number) => (
+              <CartItem key={i} product={cartItem.product} id={cartItem.id} />
+            ))}
         </ItemsWrapper>
         <BuyButton to={"/form"}>Finalizar compra</BuyButton>
       </CartCard>
