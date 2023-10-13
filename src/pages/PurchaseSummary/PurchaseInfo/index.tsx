5c5b9b470703f1c742a51b022cc79e0628bfb187
Douglas Santos <dgstos94@gmail.com>
2023-10-13 09:49:54 -0300

    feat: add due date

--- src/pages/PurchaseSummary/PurchaseInfo/index.tsx
+++ src/pages/PurchaseSummary/PurchaseInfo/index.tsx
@@ -1,5 +1,6 @@
 import { SmallParagraph } from "../../../components/Text";
 import { PurchaseInfoWrapper } from "./styles";
+import { addDays, format } from "date-fns";
 
 interface IPurchaseInfoProps {
   info: {
@@ -18,17 +19,21 @@ interface IPurchaseInfoProps {
 export default function PurchaseInfo({
   info: { amount, buyerName, street, number, complement, neighborhood, city, state, paymentType }
 }: IPurchaseInfoProps) {
+  const today = new Date();
+  const todayPlus = addDays(today, 7);
+  const formatted = format(todayPlus, "dd/MM/yyyy");
   return (
     <PurchaseInfoWrapper>
       <SmallParagraph>Valor total: {amount}</SmallParagraph>
+      <SmallParagraph>Forma de pagamento: {paymentType}</SmallParagraph>
       <SmallParagraph>Cliente: {buyerName}</SmallParagraph>
       <SmallParagraph>
         Endereço de entrega: {street}, {number}, {complement}, {neighborhood}, {city}/{state}
       </SmallParagraph>
-      <SmallParagraph></SmallParagraph>
-      <SmallParagraph></SmallParagraph>
       <SmallParagraph>Status: {paymentType === "card" ? "Aguardando envio" : "Aguardando pagamento"}</SmallParagraph>
-      <SmallParagraph>Previsão de entrega: 18/10/2023</SmallParagraph>
+      <SmallParagraph>
+        Previsão de entrega: {paymentType === "card" ? formatted : "7 dias a partir da data de pagamento do boleto"}
+      </SmallParagraph>
     </PurchaseInfoWrapper>
   );
 }
