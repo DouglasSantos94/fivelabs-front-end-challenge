9c4d49d2e813ad26c2f60a9dee8ac91ff904ea4f
Douglas Santos <dgstos94@gmail.com>
2023-10-13 01:02:37 -0300

    feat: add card number validation

--- src/schemas/formSchema.ts
+++ src/schemas/formSchema.ts
@@ -1,5 +1,8 @@
+/* eslint-disable @typescript-eslint/no-unsafe-call */
+/* eslint-disable @typescript-eslint/no-unsafe-member-access */
 import * as Yup from "yup";
 import { cpf } from "cpf-cnpj-validator";
+import validator from "validator";
 
 export const formSchema = Yup.object().shape({
   name: Yup.string().required("Nome é obrigatório"),
@@ -24,7 +27,18 @@ export const formSchema = Yup.object().shape({
   neighborhood: Yup.string().required("Digite o nome do bairro"),
   city: Yup.string().required("Digite a cidade"),
   state: Yup.string().required("Digite o estado"),
-  cardNumber: Yup.string().required("Digite o número do cartão"),
+  cardNumber: Yup.string()
+    .test({
+      name: "valid-card-number",
+      skipAbsent: true,
+      test(value = "", ctx) {
+        if (!validator.isCreditCard(value)) {
+          return ctx.createError({ message: "Número inválido!" });
+        }
+        return true;
+      }
+    })
+    .required("Digite o número do cartão"),
   owner: Yup.string().required("Digite o nome do titular"),
   valid: Yup.string().required("Digite a data de validade"),
   code: Yup.string().required("Digite o código verificador")
